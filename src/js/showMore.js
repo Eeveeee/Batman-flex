d3a81a881b91ede6cb1315d7543981b2081cc8b2
Eeveeee <fx8300gfe@gmail.com>
2020-07-03 01:40:10 +0300

    release

--- src/js/showMore.js
+++ src/js/showMore.js
@@ -1,18 +1,21 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-  setTimeout(() => {
-    window.scrollTo(0, 0);
-  }, 400);
-  const sectionMore = document.querySelector(".more");
-  const buttonMore = document.querySelector(".button-more");
+const deviceWidth = document.documentElement.clientWidth;
+if (deviceWidth > 480) {
+  document.addEventListener("DOMContentLoaded", (e) => {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 400);
+    const sectionMore = document.querySelector(".more");
+    const buttonMore = document.querySelector(".button-more");
 
-  buttonMore.addEventListener("click", () => {
-    document.body.style.overflow === "auto"
-      ? window.scrollBy({ top: -1000, behavior: "smooth" })
-      : window.scrollBy({ top: 1000, behavior: "smooth" });
-    document.body.style.overflow = `${
-      document.body.style.overflow === "auto" ? "hidden" : "auto"
-    }`;
-    buttonMore.classList.toggle("button-active");
-    sectionMore.classList.toggle("more-active");
+    buttonMore.addEventListener("click", () => {
+      document.body.style.overflow === "auto"
+        ? window.scrollBy({ top: -1000, behavior: "smooth" })
+        : window.scrollBy({ top: 1000, behavior: "smooth" });
+      document.body.style.overflow = `${
+        document.body.style.overflow === "auto" ? "hidden" : "auto"
+      }`;
+      buttonMore.classList.toggle("button-active");
+      sectionMore.classList.toggle("more-active");
+    });
   });
-});
+}
